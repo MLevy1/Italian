f3f4f305bae5e71fee59b0e88aa56b250a792726
MLevy1 <mslevy1@gmail.com>
2025-11-19 21:39:26 -0500

    better

--- app.js
+++ app.js
@@ -101,8 +101,14 @@ async function renderContent() {
                 <section class="mb-5">
                     <h3 class="mb-3">${parseBoldText(section.title)}</h3>
                     ${section.paragraphs.map(p => `
-                        ${p.id ? `<span class="unit-id h5 fw-bold me-2">${p.id}</span>` : ''}
-                        ${parseBoldText(p.content)}
+
+                        ${p.content ? `
+                            <p class="mb-3">
+                                ${p.id ? `<span class="h5 fw-bold text-info me-3 align-middle">${p.id}</span>` : ''}
+                                ${parseBoldText(p.content)}
+                            </p>
+                        ` : ''}
+
                         ${p.list ? `<ul class="list-group list-group-flush">${p.list.map(item => `<li class="list-group-item bg-dark text-light">${parseBoldText(item)}</li>`).join('')}</ul>` : ''}
                         ${p.table ? `
                             <div class="table-responsive">
